169831c7fad62e03191b9c5f063d4e0a28d5a071
LoTwT <709937065@qq.com>
2021-06-15 21:27:58 +0800

    twoSum

---
+++ 1.两数之和/twoSum.ts
@@ -0,0 +1,23 @@
+// 两重 for
+const twoSum_1 = (nums: number[], target: number): number[] => {
+    for (let i: number = 0; i < nums.length; i++) {
+        for (let j: number = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+}
+
+// map + for in
+const twoSum_2 = (nums: number[], target: number): number[] => {
+    let map = new Map<number, number>()
+
+    for (const key in nums) {
+        if (map.has(target - nums[key])) {
+            return [map.get(target - nums[key]), Number(key)]
+        } else {
+            map.set(nums[key], Number(key))
+        }
+    }
+}
